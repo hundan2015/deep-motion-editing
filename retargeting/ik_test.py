c903ab20db881adfb0f55b2ff3fe2fed1c675766
Symbolic <sym12138@hotmail.com>
2024-10-09 08:45:34 +0000

    28.9loss version

--- retargeting/ik_test.py
+++ retargeting/ik_test.py
@@ -28,6 +28,7 @@ from pytorch3d.transforms import (
     matrix_to_quaternion,
 )
 from einops import einsum
+from hmrutils.geo.quaternion import qbetween, qslerp, qinv, qmul, qrot
 
 
 def get_parent_chain(parents: list, index: int, stop: int) -> list:
@@ -48,86 +49,19 @@ def get_id_from_joint_names(name_list: list, names: list) -> list:
     return res
 
 
-def transform_from_quaternion(quater: torch.Tensor):
-    qw = quater[..., 0]
-    qx = quater[..., 1]
-    qy = quater[..., 2]
-    qz = quater[..., 3]
+def get_obj(positions, name, index=0):
+    import trimesh
 
-    x2 = qx + qx
-    y2 = qy + qy
-    z2 = qz + qz
-    xx = qx * x2
-    yy = qy * y2
-    wx = qw * x2
-    xy = qx * y2
-    yz = qy * z2
-    wy = qw * y2
-    xz = qx * z2
-    zz = qz * z2
-    wz = qw * z2
+    def make_cube(size, position):
+        cube = trimesh.creation.box(extents=[size, size, size])
+        target_position = position  # 指定位置
+        cube.apply_translation(target_position)
+        return cube
 
-    m = torch.empty(quater.shape[:-1] + (3, 3), device=quater.device)
-    m[..., 0, 0] = 1.0 - (yy + zz)
-    m[..., 0, 1] = xy - wz
-    m[..., 0, 2] = xz + wy
-    m[..., 1, 0] = xy + wz
-    m[..., 1, 1] = 1.0 - (xx + zz)
-    m[..., 1, 2] = yz - wx
-    m[..., 2, 0] = xz - wy
-    m[..., 2, 1] = yz + wx
-    m[..., 2, 2] = 1.0 - (xx + yy)
-
-    return m
-
-
-def forward(
-    topology,
-    rotation: torch.Tensor,
-    position: torch.Tensor,
-    offset: torch.Tensor,
-    order="xyz",
-    quater=True,
-    world=True,
-):
-    """
-    rotation should have shape batch_size * Joint_num * (3/4) * Time
-    position should have shape batch_size * 3 * Time
-    offset should have shape batch_size * Joint_num * 3
-    output have shape batch_size * Time * Joint_num * 3
-    """
-    if not quater and rotation.shape[-2] != 3:
-        raise Exception("Unexpected shape of rotation")
-    if quater and rotation.shape[-2] != 4:
-        raise Exception("Unexpected shape of rotation")
-    rotation = rotation.permute(0, 3, 1, 2)
-    position = position.permute(0, 2, 1)
-    result = torch.empty(rotation.shape[:-1] + (3,), device=position.device)
-
-    norm = torch.norm(rotation, dim=-1, keepdim=True)
-    # norm[norm < 1e-10] = 1
-    rotation = rotation / norm
-
-    if quater:
-        transform = transform_from_quaternion(rotation)
-
-    offset = offset.reshape((-1, 1, offset.shape[-2], offset.shape[-1], 1))
-
-    result[..., 0, :] = position
-    for i, pi in enumerate(topology):
-        if pi == -1:
-            assert i == 0
-            continue
-
-        transform[..., i, :, :] = torch.matmul(
-            transform[..., pi, :, :].clone(), transform[..., i, :, :].clone()
-        )
-        result[..., i, :] = torch.matmul(
-            transform[..., i, :, :], offset[..., i, :, :]
-        ).squeeze()
-        if world:
-            result[..., i, :] += result[..., pi, :]
-    return result
+    cube0 = make_cube(1, positions[..., 0, :][index])
+    for i in range(1, positions.shape[-2]):
+        cube0 += make_cube(1, positions[..., i, :][index])
+    cube0.export(name)
 
 
 def example(
@@ -139,73 +73,158 @@ def example(
     anim, name, ftime = BVH.load(input_file_dir)
     clip_len, n_joints, _ = anim.rotations.qs.shape
 
-    # region forward test
-    temp_result = forward(
-        anim.parents,
-        torch.from_numpy(anim.rotations.qs)
-        .permute(1, 2, 0)
-        .unsqueeze(0)
-        .to(torch.float32),
-        torch.from_numpy(anim.positions)
-        .permute(1, 2, 0)[0]
-        .unsqueeze(0)
-        .to(torch.float32),
-        torch.from_numpy(anim.offsets).unsqueeze(0).to(torch.float32),
-    )
-    # endregion
-
     count = 1
 
     # BVH anim.rotations.qs + offset to local mat. Need a fk like procedure.
     rotation_mat = quaternion_to_matrix(
         torch.from_numpy(anim.rotations.qs)
-    )  # (324,29,3,3)
+    )  # (324,29,3,3),PASSED
     offset = (
         torch.from_numpy(anim.offsets).unsqueeze(0).repeat(clip_len, 1, 1)
-    )  # (324,29,3)
-    # rotation_mat = rotation_mat.transpose(-1,-2)
-    local_mat = matrix.get_TRS(rotation_mat, offset)  # (324,29,4,4)
+    )  # (324,29,3),PASSED
+    local_mat = matrix.get_TRS(rotation_mat, offset)  # (324,29,4,4),PASSED
 
-    fk_mat = matrix.forward_kinematics(local_mat, anim.parents)  # (324,29,4,4)
-    global_rot = matrix.get_rotation(fk_mat).clone()  # (324,29,3,3)
+    fk_mat = matrix.forward_kinematics(local_mat, anim.parents)  # (324,29,4,4),PASSED
+    global_rot = matrix.get_rotation(fk_mat).clone()  # (324,29,3,3),CONFERMED
+    positions = matrix.get_position(fk_mat).clone()
+    # region Trimesh visiualize
+    # positions = matrix.get_position(fk_mat)
+    # hip_points = matrix.get_position(fk_mat)[
+    #     ..., get_id_from_joint_names(["Hips"], name)[0], :
+    # ].to(torch.float32)
+
+    # get_obj(positions, "fuckme1.obj")
+    # endregion
 
     if target_points == None:
         # target_points = anim.positions[:, get_id_from_joint_names(["Head"], name)[0], :]
         target_points = matrix.get_position(fk_mat)[
             ..., get_id_from_joint_names(["Head"], name)[0], :
         ].to(torch.float32)
-        # target_points = torch.from_numpy(target_points).to(torch.float32)
+        # target_points = target_points.unsqueeze(-1)
+        # target_points = torch.zeros_like(target_points)
 
-    # Get chain from end point and parent.
+    # region my ik test
     chain = get_parent_chain(
         anim.parents,
         get_id_from_joint_names(["LeftHand"], name)[0],
-        get_id_from_joint_names(["LeftShoulder"], name)[0],
+        get_id_from_joint_names(["Spine2"], name)[0],
     )  # PASS
-    target_ind = (list(range(len(chain))))[-count:]  # PASS
+    local_mat_short = local_mat[..., chain, :, :].clone()
+    local_mat_short[..., 0, :, :] = fk_mat[..., chain[0], :, :].clone()
+    parent_short = [i - 1 for i in range(len(chain))]
 
-    IK_solver = CCD_IK(
-        local_mat=local_mat,
-        parent=anim.parents,
-        target_ind=target_ind,
-        target_pos=target_points,  # local postion
-        target_rot=global_rot[..., chain[-count:], :, :],
-        kinematic_chain=chain,
-        max_iter=2,
+    fk_mat_short = matrix.forward_kinematics(local_mat_short, parent_short)
+    positions_short = matrix.get_position(fk_mat_short)
+    rotations_short = matrix.get_rotation(local_mat_short)
+    root_position = positions_short[:, 0, :]
+    original_position = positions_short[:, 1, :]
+    target_position = target_points.clone()
+    quat = matrix_to_quaternion(rotations_short[:, 1])
+
+    # region Torch optimize
+    # epoch = 100
+    # target = target_position
+    # loss_fn = torch.nn.MSELoss()  # 使用均方误差损失
+    # for step in range(epoch):
+    #     var = torch.nn.Parameter(quat.clone().detach().requires_grad_())
+    #     optimizer = torch.optim.Adamax([var], lr=0.01)
+    #     optimizer.zero_grad()  # 清除前一轮的梯度
+    #     local_mat_temp = local_mat_short.clone()
+    #     local_mat_temp[:, 1, :3, :3] = quaternion_to_matrix(var)
+    #     end_pos_temp = matrix.get_position(
+    #         matrix.forward_kinematics(local_mat_temp, parent_short)
+    #     )[:, -1]
+    #     loss = loss_fn(end_pos_temp, target)
+    #     loss.backward()
+    #     optimizer.step()
+    #     quat = var.clone().detach()
+    #     if step % 10 == 0:
+    #         print(f"Step {step}, Loss: {loss.item()}")
+
+    # chain_rotmat = matrix.get_rotation(local_mat_short)
+    # chain_rotmat[:, 1] = quaternion_to_matrix(quat)
+    # local_mat[..., chain[1:], :-1, :-1] = chain_rotmat[..., 1:, :, :]  # (B, L, J, 3, 3)
+    # endregion
+    # region traditional
+    solved_pos_target_quat = qmul(
+        qbetween(
+            original_position - root_position, target_position - original_position
+        ),
+        quat,
     )
 
-    chain_local_mat = IK_solver.solve()
-    chain_rotmat = matrix.get_rotation(chain_local_mat)
-    local_mat[..., chain[1:], :-1, :-1] = chain_rotmat[..., 1:, :, :]  # (B, L, J, 3, 3)
+    x_vec = torch.zeros((quat.shape[:-1] + (3,)), device=quat.device)
+    x_vec[..., 0] = 1.0
+    x_vec_sum = torch.zeros_like(x_vec)
+    y_vec = torch.zeros((quat.shape[:-1] + (3,)), device=quat.device)
+    y_vec[..., 1] = 1.0
+    y_vec_sum = torch.zeros_like(y_vec)
+    x_vec_sum += qrot(solved_pos_target_quat, x_vec)
+    y_vec_sum += qrot(solved_pos_target_quat, y_vec)
+
+    x_vec_avg = matrix.normalize(x_vec_sum / count)
+    y_vec_avg = matrix.normalize(y_vec_sum / count)
+    z_vec_avg = torch.cross(x_vec_avg, y_vec_avg, dim=-1)
+    solved_rot = torch.stack([x_vec_avg, y_vec_avg, z_vec_avg], dim=-1)  # column
+    parent_rot = matrix.get_rotation(fk_mat_short)[..., 0, :, :]
+    solved_local_rot = matrix.get_mat_BtoA(parent_rot, solved_rot)
+
+    local_mat_short[..., 1, :-1, :-1] = solved_local_rot
+    new_fk_mat_short = matrix.forward_kinematics(local_mat_short, parent_short)
+    new_position_short = matrix.get_position(new_fk_mat_short)
+
+    print(
+        torch.norm(
+            torch.nn.functional.normalize(
+                new_position_short[:, -1, :] - new_position_short[:, 1, :]
+            )
+            - torch.nn.functional.normalize(target_points - new_position_short[:, 1, :])
+        )
+    )
+    # endregion
+    # endregion
+
+    # region Original CCD IK part
+    # # Get chain from end point and parent.
+    # chain = get_parent_chain(
+    #     anim.parents,
+    #     get_id_from_joint_names(["LeftHand"], name)[0],
+    #     get_id_from_joint_names(["Spine2"], name)[0],
+    # )  # PASS
+    # target_ind = (list(range(len(chain))))[-count:]  # PASS
+
+    # IK_solver = CCD_IK(
+    #     local_mat=local_mat,
+    #     parent=anim.parents,
+    #     target_ind=target_ind,
+    #     target_pos=target_points,  # local postion
+    #     target_rot=global_rot[..., chain[-count:], :, :],
+    #     # target_rot=None,
+    #     kinematic_chain=chain,
+    #     max_iter=10,
+    # )
+
+    # chain_local_mat = IK_solver.solve()
+    # chain_rotmat = matrix.get_rotation(chain_local_mat)
+    # local_mat[..., chain[1:], :-1, :-1] = chain_rotmat[..., 1:, :, :]  # (B, L, J, 3, 3)
+    # endregion
+
+    # region Eye test
+    # local_mat[..., get_id_from_joint_names(["LeftForeArm"], name)[0], :3, :3] = (
+    #     torch.eye(3, device=local_mat.device)
+    # )
+    # endregion
 
     rotation_mat = matrix.get_rotation(local_mat)
     rotation = matrix_to_quaternion(rotation_mat)
 
     new_fk_mat = matrix.forward_kinematics(local_mat, anim.parents)
     position = matrix.get_position(new_fk_mat)
+    get_obj(position, "fuckme2.obj", 77)
 
     anim.rotations = Quaternions(rotation.numpy())
-    anim.positions = position.numpy()
+    # anim.positions = position.numpy()
 
     BVH.save(output_path, anim, name, ftime)
 
@@ -213,6 +232,6 @@ def example(
 if __name__ == "__main__":
     example(
         "./datasets/Mixamo/Aj/Disappointed.bvh",
-        "./examples/intra_structure/fuck.bvh",
+        "./examples/intra_structure/fuck3.bvh",
     )
     print("Finished!")
